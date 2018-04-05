5a571734ca4fad38327bbe10bd3e1f3561306eb7
taihelsel <tai.bayard@gmail.com>
2018-04-05 04:29:38 -0700

    stubbed out html for landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -14,8 +14,44 @@ const Landing = () => {
             </div>
             <p id="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, nemo accusantium ad ea expedita voluptate in sapiente unde, fugit esse veritatis maxime natus commodi reprehenderit alias quos enim? Hic, commodi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sint illo quasi, cum cumque quibusdam assumenda accusamus, doloremque nostrum quis voluptate magnam totam nam porro autem earum necessitatibus laborum quisquam.</p>
         </div>
-        <div id="landing-skills"></div>
-        <a id="landing-portfolio-btn"></a>
+        <div id="landing-skills">
+            <h1 id="landing-skills-label">Skills</h1>
+            <ul className="skills-list">
+                <li className="skill-label">Front End</li>
+                <li className="skill-item">JavaScript</li>
+                <li className="skill-item">JQuery</li>
+                <li className="skill-item">HTML</li>
+                <li className="skill-item">CSS3</li>
+                <li className="skill-label">Frameworks / Templates</li>
+                <li className="skill-item">React</li>
+                <li className="skill-item">EJS</li>
+                <li className="skill-item">Jinja2</li>
+            </ul>
+            <ul className="skills-list">
+                <li className="skill-label">Back End</li>
+                <li className="skill-item">PostgreSQL</li>
+                <li className="skill-item">Sequelize</li>
+                <li className="skill-item">MongoDB</li>
+                <li className="skill-item">Mongoose</li>
+                <li className="skill-item">Django</li>
+                <li className="skill-label">Python</li>
+                <li className="skill-item">Python3</li>
+                <li className="skill-item">Django</li>
+                <li className="skill-item">Tensorflow</li>
+            </ul>
+            <ul className="skills-list">
+                <li className="skill-label">Version Control</li>
+                <li className="skill-item">Git / GitHub</li>
+                <li className="skill-item">Workflow</li>
+                <li className="skill-item">Agile Methodoloiges</li>
+                <li className="skill-item">Trello</li>
+                <li className="skill-item">OS</li>
+                <li className="skill-label">Windows</li>
+                <li className="skill-item">Mac OS</li>
+                <li className="skill-item">Linux</li>
+            </ul>
+        </div>
+        <div id="landing-portfolio-btn"><a>Portfolio</a></div>
       </section>
     );
 }
