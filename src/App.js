732712db54f4939db9ec33a2f7daaaa1b06ddf18
taihelsel <tai.bayard@gmail.com>
2018-04-04 18:23:42 -0700

    added landing component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/*React Components*/
+import Landing from "./components/Landing/Landing.js";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Empy App
+        <Landing />
       </div>
     );
   }
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../.././App.css';
+
+const Landing = () => {
+    return (
+      <section id="landing-page">
+        landing page
+      </section>
+    );
+}
+
+export default Landing;
