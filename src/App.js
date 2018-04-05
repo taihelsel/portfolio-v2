e46bb9846fdc793b5e86933447f4603885c3715e
taihelsel <tai.bayard@gmail.com>
2018-04-05 06:10:10 -0700

    added projects page and jquery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import $ from 'jquery'; 
 /*React Components*/
 import Landing from "./components/Landing/Landing.js";
+import Projects from  "./components/Projects/Projects.js";
 class App extends Component {
+  componentDidMount(){
+    $("#landing-portfolio-btn").click(()=>{
+      $('html, body').animate({
+        scrollTop: $("#projects-page").offset().top
+      }, 1000);
+    });
+  }
   render() {
     return (
       <div className="App">
         <Landing />
+        <Projects />
       </div>
     );
   }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../.././App.css';
+
+const Projects = () => {
+    return (
+      <section id="projects-page">
+        projects here
+      </section>
+    );
+}
+
+export default Projects;
