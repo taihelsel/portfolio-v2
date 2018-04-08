322f29af1f3a87d5fd9a60aff8eba3c7495ae42d
taihelsel <tai.bayard@gmail.com>
2018-04-07 22:42:09 -0700

    added animations for projects page

--- src/App.js
+++ src/App.js
@@ -26,6 +26,20 @@ class App extends Component {
         scrollTop: $("#projects-page").offset().top
       }, 1000);
     });
+    window.addEventListener("scroll", () => {
+      const projects = document.getElementById("projects-page");
+      const x = document.getElementsByClassName("projects-item");
+      const offset =  window.screen.height/3;
+      let shouldAnimate = x[0].classList.contains("animation-fadein");
+      const projects_top = projects.getBoundingClientRect().top;
+      console.log(projects_top);
+      if(projects_top < offset && !(shouldAnimate)){
+        x[0].classList.add("animation-fadein");
+        x[1].classList.add("animation-fadein");
+        x[2].classList.add("animation-leftin");
+        x[3].classList.add("animation-rightin"); 
+      }
+  })
   }
   render() {
     return (
