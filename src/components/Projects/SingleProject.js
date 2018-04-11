cadd613843d91e77b533e744680b82c3bd4f1ff5
taihelsel <tai.bayard@gmail.com>
2018-04-11 16:10:37 -0700

    moved X btn outside of single projects component and into parent component

--- src/components/Projects/SingleProject.js
+++ src/components/Projects/SingleProject.js
@@ -1,22 +1,9 @@
 import React, { Component } from 'react';
 import '../.././App.css';
 class SingeProject extends Component {
-    handleCloseClick = (e) =>{
-        const x = document.getElementsByClassName("projects-item");
-        x[0].classList.remove("animation-leftout");
-        x[1].classList.remove("animation-rightout");
-        x[2].classList.remove("animation-leftout");
-        x[3].classList.remove("animation-rightout");
-        x[0].classList.add("animation-leftin");
-        x[1].classList.add("animation-rightin");
-        x[2].classList.add("animation-leftin");
-        x[3].classList.add("animation-rightin");
-        document.getElementById("single-project").setAttribute("style","visibility:hidden;opacity:0;");
-    }
     render() {
         return (
             <div id="single-project" style={{visibility:"hidden"}}>
-                <h1 onClick={this.handleCloseClick} className="close-btn">X</h1>
                 {this.props.name}
                 <br />
                 {this.props.role}
